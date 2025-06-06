89fa4fa520877c7888406460f2875b4a3de4ef39
Syahwa Wahyu <syahwasupercell6@gmail.com>
2025-06-06 19:47:31 +0700

    +file globals

---
+++ globals.mjs
@@ -0,0 +1,20 @@
+setTimeout(() => {
+    console.info("Ta daaa!");
+    console.info("This is a delayed message.");
+}, 5000)
+
+setTimeout(() => {
+    console.info("1");
+}, 1000);
+
+setTimeout(() => {
+    console.info("2");
+}, 2000);
+
+setTimeout(() => {
+    console.info("3");
+}, 3000);
+
+setTimeout(() => {
+    console.info("4");
+}, 4000);
